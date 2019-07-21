514a23b6f339565b22d02e03366ad51b96269700
Przemysław Woźniak <inz.przemyslaw.wozniak@gmail.com>
2019-07-21 15:55:25 +0200

    Starting point for Hibernate lessons.

---
+++ src/main/java/pl/sda/javawwa18/SimpleMovie.java
@@ -0,0 +1,40 @@
+package pl.sda.javawwa18;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "movies")    //nadpisanie domyslnej konfiguracji celem ustalenia nazwy tabeli
+public class SimpleMovie {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @Column(nullable = false)
+    String title;
+
+    public SimpleMovie() {
+    }
+
+    public SimpleMovie(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
