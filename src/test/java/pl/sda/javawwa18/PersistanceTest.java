be8ac3c07e8f9d9ebb8154150bb5d04618c50b3f
Przemysław Woźniak <inz.przemyslaw.wozniak@gmail.com>
2019-07-27 12:10:25 +0200

    Ex 7 - answer

--- src/test/java/pl/sda/javawwa18/PersistanceTest.java
+++ src/test/java/pl/sda/javawwa18/PersistanceTest.java
@@ -7,11 +7,14 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 public class PersistanceTest {
 
@@ -28,8 +31,9 @@ public class PersistanceTest {
                 .buildSessionFactory();
     }
 
+    @Ignore
     @Test
-    public void saveMovies() {
+    public void saveSimpleMovies() {
         SimpleMovie movie = new SimpleMovie("Ogniem i mieczem");
         SimpleMovie movie2 = new SimpleMovie("Planet Earth II");
         try(Session session = sessionFactory.openSession()) {
@@ -40,8 +44,9 @@ public class PersistanceTest {
         }
     }
 
-    @Test(dependsOnMethods = "saveMovies")
-    public void readMovies() {
+    @Ignore
+    @Test(dependsOnMethods = "saveSimpleMovies")
+    public void readSimpleMovies() {
         try(Session session = sessionFactory.openSession()) {
             List<SimpleMovie> movies = session.createQuery("from SimpleMovie", SimpleMovie.class).list();
             assertEquals(movies.size(), 2);
@@ -50,7 +55,38 @@ public class PersistanceTest {
         }
     }
 
+    //Cwiczenie 7
+    @Test
+    public void saveMovies() {
+        Movie movie = new Movie();
+        movie.setTitle("Smierc w Wenecji");
+        movie.setDescription("Zajebisty film :)");
+        movie.setGenre(MovieGenre.ACTION);
+        movie.setReleaseDate(LocalDate.of(2001, 4, 1));
 
+        try(Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.persist(movie);
+            tx.commit();
+        }
+    }
+
+    @Test(dependsOnMethods = "saveMovies")
+    public void readMovies() {
+        try(Session session = sessionFactory.openSession()) {
+            List<Movie> movies = session.createQuery("from Movie", Movie.class).list();
+            assertEquals(movies.size(), 1);
+            Movie movie = movies.get(0);
+            assertNotNull(movie.getMovieId());
+            assertEquals(movie.getTitle(), "Smierc w Wenecji");
+            assertEquals(movie.getGenre(), MovieGenre.ACTION);
+            //test time
+            assertEquals(movie.getReleaseDate(), LocalDate.of(2001, 4, 1));
+            assertNotNull(movie.getRentedTimes());
+            //test default
+            assertEquals(movie.getRentedTimes(), 0);
+        }
+    }
 
 }
 
